26d1beaa3bd0d7ac0b099bf48c297f22c853629d
devyanarisky <riskidevyana@gmail.com>
2024-11-04 09:33:00 +0700

    Add files via upload

---
+++ Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kelipatan;
+
+/**
+ *
+ * @author USER
+ * NAMA: Devyana Risky Putri Andini
+ * MATA KULIAH: PBO1
+ * NIM: 23176023
+ * PRODI: Sistem Informasi
+ * SEMESTER: 3
+ * 
+ */
+
+public class Kelipatan {
+    public static void main(String[] args) {
+        double angka = 3.5;
+        while (angka <= 35){
+            System.out.println(angka);
+            angka += 3.5;
+        }
+    }
+}
+    
+
